00047d8e3693f7fc067449936210fe3472ead3aa
lucas-backDev <lucas.cgomesb19@gmail.com>
2025-01-20 10:01:28 -0300

    feat: estruturando o site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,58 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <Image
-        src="/next.svg"
-        alt="Next.js logo"
-        width={180}
-        height={38}
-        priority
-      />
+
+      <div className="header">
+        <Image
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+
+        <div>
+          <h1>Lucas Gomes</h1>
+          <h2>Full Stack Developer</h2>
+        </div>
+
+      </div>
+
+      <div className="skills">
+        <h3>Skills</h3>
+        <p></p>
+        <div className="skillslevel">
+
+        </div>
+
+        <div className="infos">
+          <h3>Languages</h3>
+
+          <div className="languages-info">
+            <span>🇺🇸 EN - Itermediate</span>
+            <span>🇺🇸 PT-BR - Native</span>
+          </div>
+          
+          <h3>Education</h3>
+
+          <div className="education-info">
+            <span>🎓</span>
+            <span>Studying Computer Science Bachelor's Degree - Universidade Paulista</span>
+          </div>
+
+        </div>
+
+        <div className="buttons">
+          
+          <div className="social">
+
+          </div>
+
+          <button>Contact-me</button>
+
+        </div>
+
+      </div>
     </main>    
   );
 }
