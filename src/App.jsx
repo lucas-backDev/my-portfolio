ddcd00a6a8e26b4450693837c633c9da14a22cab
lucas-backDev <lucas.cgomesb19@gmail.com>
2025-01-22 14:20:11 -0300

    feat: starting header and navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/**
+ *  @copyright 2024 lucas-backDev
+ *  @license Apache-2.0
+ */
+
+
+/**
+ * Componentes
+ */
+import Header from "./components/Header"
+
+
+const App = () => {
+    return <Header />
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,26 @@
+/**
+ *  @copyright 2024 lucas-backDev
+ *  @license Apache-2.0
+ */
+
+
+/**
+ * Node modules
+ */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+
+/**
+ * Componentes
+ */
 import './index.css'
-import App from './App.jsx'
+
+
+/**
+ * link CSS
+ */
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
