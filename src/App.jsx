1eee2cfa5badeab8fcff0e907011a6718c01ce6e
lucas-backDev <lucas.cgomesb19@gmail.com>
2025-02-13 10:59:37 -0300

    feat: seção work para apresentar projetos

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,11 @@
 /**
  * Componentes
  */
-import Header from "./components/Header"
-import Hero from "./components/Hero"
-import About from "./components/About"
-import Skill from "./components/Skill"
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skill from "./components/Skill";
+import Work from "./components/Work";
 
 
 const App = () => {
@@ -21,6 +22,7 @@ const App = () => {
             <Hero />
             <About />
             <Skill />
+            <Work />
         </main>
       </>
 )
---
+++ src/components/Work.jsx
@@ -0,0 +1,50 @@
+/**
+ *  @copyright 2025 lucas-backDev
+ *  @license Apache-2.0
+ */
+
+
+import ProjectCard from "./ProjectCard";
+
+
+const works = [
+    {
+      imgSrc: '',
+      title: 'I\'m working on it',
+      tags: ['Developing'],
+      projectLink: 'https://github.com/lucas-backDev'
+    },
+    
+  ];
+
+
+const Work = () => {
+    return (
+        <section 
+        id="work"
+        className="section"
+        >
+            <div className="container">
+
+                <h2 className="headliner-2 mb-8">
+                    My portfolio highlights
+                </h2>
+
+                <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(autofill,_minmax(280px,_1fr))]">
+                    {works.map(({ imgSrc, title, tags, projectLink }, key) => (
+                        <ProjectCard 
+                        key={key}
+                        imgSrc={imgSrc}
+                        title={title}
+                        tags={tags}
+                        projectLink={projectLink}
+                        />
+                    ))}
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Work
