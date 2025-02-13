c8b64e056ab12eefd971520f36f71d242e214d76
lucas-backDev <lucas.cgomesb19@gmail.com>
2025-02-13 11:32:38 -0300

    feat: seção de reviews e feedbacks

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
 import Work from "./components/Work";
+import Review from "./components/Review";
 
 
 const App = () => {
@@ -23,6 +24,7 @@ const App = () => {
             <About />
             <Skill />
             <Work />
+            <Review />
         </main>
       </>
 )
