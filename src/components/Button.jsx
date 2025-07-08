8671a3baedb24e348cd94e24c18cb24816d72f9f
Lucas Gomes <lucas.cgomesb19@gmail.com>
2025-07-08 01:14:32 -0300

    download cv button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,119 +1,90 @@
-/**
- *  @copyright 2025 lucas-backDev
- *  @license Apache-2.0
- */
+"use client"
 
+interface ButtonProps {
+    href?: string
+    target?: string
+    label: string
+    icon?: string
+    classes?: string
+    onClick?: () => void
+}
 
-/**
- * Node modules
- */
-import PropTypes from "prop-types";
-
-
-/**
- * Primary Button
- */
-const ButtonPrimary = ({
-    href,
-    target = '_self',
-    label,
-    icon,
-    classes
-}) => {
-    if (href) {
+const ButtonPrimary = ({ href, target = "_self", label, icon, classes = "", onClick }: ButtonProps) => {
+    if (onClick) {
         return (
-            <a 
-            href={href}
-            target={target}
-            className={"btn btn-primary " + classes}
-            >
+            <button className={`btn btn-primary ${classes}`} onClick={onClick}>
                 {label}
-
-                {icon ? 
-                <span className="material-symbols-rounded" aria-hidden="true">
-                    {icon}
-                </span>
-                : undefined
-                }
-            </a>
-        ) 
-    } else {
-        return (
-            <button className={"btn btn-primary " + classes}>
-                {label}
-
-                {icon ? 
-                <span className="material-symbols-rounded" aria-hidden="true">
-                    {icon}
-                </span>
-                : undefined
-                }
+                {icon && (
+                    <span className="material-symbols-rounded" aria-hidden="true">
+                        {icon}
+                    </span>
+                )}
             </button>
         )
     }
-}
 
-ButtonPrimary.propTypes = {
-    label: PropTypes.string.isRequired,
-    href: PropTypes.string,
-    target: PropTypes.string,
-    icon: PropTypes.string,
-    classes: PropTypes.string
-}
-
-
-/**
- * Outline Button
- */
-const ButtonOutline = ({
-    href,
-    target = '_self',
-    label,
-    icon,
-    classes
-}) => {
     if (href) {
         return (
-            <a 
-            href={href}
-            target={target}
-            className={"btn btn-outline " + classes}
-            >
+            <a href={href} target={target} className={`btn btn-primary ${classes}`}>
                 {label}
-
-                {icon ? 
-                <span className="material-symbols-rounded" aria-hidden="true">
-                    {icon}
-                </span>
-                : undefined
-                }
+                {icon && (
+                    <span className="material-symbols-rounded" aria-hidden="true">
+                        {icon}
+                    </span>
+                )}
             </a>
-        ) 
-    } else {
-        return (
-            <button className={"btn btn-outline " + classes}>
-                {label}
+        )
+    }
 
-                {icon ? 
+    return (
+        <button className={`btn btn-primary ${classes}`}>
+            {label}
+            {icon && (
                 <span className="material-symbols-rounded" aria-hidden="true">
                     {icon}
                 </span>
-                : undefined
-                }
+            )}
+        </button>
+    )
+}
+
+const ButtonOutline = ({ href, target = "_self", label, icon, classes = "", onClick }: ButtonProps) => {
+    if (onClick) {
+        return (
+            <button className={`btn btn-outline ${classes}`} onClick={onClick}>
+                {label}
+                {icon && (
+                    <span className="material-symbols-rounded" aria-hidden="true">
+                        {icon}
+                    </span>
+                )}
             </button>
         )
     }
+
+    if (href) {
+        return (
+            <a href={href} target={target} className={`btn btn-outline ${classes}`}>
+                {label}
+                {icon && (
+                    <span className="material-symbols-rounded" aria-hidden="true">
+                        {icon}
+                    </span>
+                )}
+            </a>
+        )
+    }
+
+    return (
+        <button className={`btn btn-outline ${classes}`}>
+            {label}
+            {icon && (
+                <span className="material-symbols-rounded" aria-hidden="true">
+                    {icon}
+                </span>
+            )}
+        </button>
+    )
 }
 
-ButtonOutline.propTypes = {
-    label: PropTypes.string.isRequired,
-    href: PropTypes.string,
-    target: PropTypes.string,
-    icon: PropTypes.string,
-    classes: PropTypes.string
-}
-
-export {
-    ButtonPrimary,
-    ButtonOutline
-}
+export { ButtonPrimary, ButtonOutline }
