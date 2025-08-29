f5bed5715064170e59cd80faaca60270bd15790d
Lucas Gomes <lucas.cgomesb19@gmail.com>
2025-08-29 11:27:18 -0300

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,72 +11,72 @@ import ProjectCard from "./ProjectCard";
 
 
 const works = [
-    {
-      imgSrc: '/images/lpporto.png',
-      title: 'Landing Page da Porto Seguro Bank, feito para a corretora Espec Seguros',
-      tags: ['Landing Page', 'WordPress', 'Elementor'],
-      projectLink: 'https://especseguros.com.br/lp_portoseguro/'
-    },
-    {
-      imgSrc: '',
-      title: 'I\'m working on it',
-      tags: ['Developing'],
-      projectLink: 'https://github.com/lucas-backDev'
-    },
-    {
-        imgSrc: '',
-        title: 'I\'m working on it',
-        tags: ['Developing'],
-        projectLink: 'https://github.com/lucas-backDev'
-    },
-    {
-        imgSrc: '',
-        title: 'I\'m working on it',
-        tags: ['Developing'],
-        projectLink: 'https://github.com/lucas-backDev'
-      },
-      {
-        imgSrc: '',
-        title: 'I\'m working on it',
-        tags: ['Developing'],
-        projectLink: 'https://github.com/lucas-backDev'
-      },
-      {
-        imgSrc: '',
-        title: 'I\'m working on it',
-        tags: ['Developing'],
-        projectLink: 'https://github.com/lucas-backDev'
-      },
-  ];
+  {
+    imgSrc: '/images/lpporto.png',
+    title: 'Landing Page da Porto Seguro Bank, feito para a corretora Espec Seguros',
+    tags: ['Landing Page', 'WordPress', 'Elementor'],
+    projectLink: 'https://especseguros.com.br/lp_portoseguro/'
+  },
+  {
+    imgSrc: '',
+    title: 'Landing Page MVituzzo',
+    tags: ['Landing Page', 'React', 'NextJS', 'TailwindCSS'],
+    projectLink: 'https://github.com/lucas-backDev'
+  },
+  {
+    imgSrc: '',
+    title: 'I\'m working on it',
+    tags: ['Developing'],
+    projectLink: 'https://github.com/lucas-backDev'
+  },
+  {
+    imgSrc: '',
+    title: 'I\'m working on it',
+    tags: ['Developing'],
+    projectLink: 'https://github.com/lucas-backDev'
+  },
+  {
+    imgSrc: '',
+    title: 'I\'m working on it',
+    tags: ['Developing'],
+    projectLink: 'https://github.com/lucas-backDev'
+  },
+  {
+    imgSrc: '',
+    title: 'I\'m working on it',
+    tags: ['Developing'],
+    projectLink: 'https://github.com/lucas-backDev'
+  },
+];
 
 
 const Work = () => {
-    return (
-        <section 
-        id="work"
-        className="section"
-        >
-            <div className="container">
+  return (
+    <section
+      id="work"
+      className="section"
+    >
+      <div className="container">
 
-                <h2 className="headline-2 mb-8 reveal-up">
-                    My portfolio highlights
-                </h2>
+        <h2 className="headline-2 mb-8 reveal-up">
+          My portfolio highlights
+        </h2>
 
-                <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
-                    {works.map(({ imgSrc, title, tags, projectLink }, key) => (
-                        <ProjectCard 
-                        key={key}
-                        imgSrc={imgSrc}
-                        title={title}
-                        tags={tags}
-                        projectLink={projectLink}
-                        />
-                    ))}
-                </div>
+        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
+          {works.map(({ imgSrc, title, tags, projectLink }, key) => (
+            <ProjectCard
+              key={key}
+              imgSrc={imgSrc}
+              title={title}
+              tags={tags}
+              projectLink={projectLink}
+            />
+          ))}
+        </div>
 
-            </div>
-        </section>
-    )
+      </div>
+    </section>
+  )
 }
 
 export default Work
